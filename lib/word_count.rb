f96d69d5150241abe4d6f5b2a2f93756182af51e
Shannon Mulloy <smulloy21@gmail.com>
2015-08-07 10:29:35 -0700

    pass spec to include words ending in 've 's and 'd

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,18 +5,18 @@ class String
 		count = 0
 		output = ""
 		sentence.each() do |wd|
-			if wd == small_word
+			if wd == small_word || wd == small_word + "'s" || wd == small_word + "'d" || wd == small_word + "'ve"
 				count += 1
 			end
 		end
 		if count > 0
 			if count > 1
-				output = "\'" + word.capitalize() + "\' occurs in \'" + self + "\' " + count.to_s + " times."
+				output = "\"" + word.capitalize() + "\" occurs in \"" + self + "\" " + count.to_s + " times."
 			else
-				output = "\'" + word.capitalize() + "\' occurs in \'" + self + "\' " + count.to_s + " time."
+				output = "\"" + word.capitalize() + "\" occurs in \"" + self + "\" " + count.to_s + " time."
 			end
 		else
-			output = "\'" + word.capitalize() + "\' does not occur in \'" + self + "\'"
+			output = "\"" + word.capitalize() + "\" does not occur in \"" + self + "\""
 		end
 		output
 	end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,18 +3,18 @@ require('word_count')
 
 describe('String#word_count')  do 
 	it("checks how many instances of a word appear in a string") do
-		expect("Can't hurry love".word_count('love')).to(eq("'Love' occurs in 'Can't hurry love' 1 time."))
+		expect("Can't hurry love".word_count('love')).to(eq("\"Love\" occurs in \"Can't hurry love\" 1 time."))
 	end
 	it("checks for instances of a word regardless of case") do
-		expect("Can't hurry love".word_count("can't")).to(eq("'Can't' occurs in 'Can't hurry love' 1 time."))
+		expect("Can't hurry love".word_count("can't")).to(eq("\"Can't\" occurs in \"Can't hurry love\" 1 time."))
 	end
 	it("checks for instances of a word regardless of punctuation") do
-		expect("My momma said, you can't hurry love".word_count("said")).to(eq("'Said' occurs in 'My momma said, you can't hurry love' 1 time."))
+		expect("My momma said, you can't hurry love".word_count("said")).to(eq("\"Said\" occurs in \"My momma said, you can't hurry love\" 1 time."))
 	end
 	it("reports multiple instances of a word within a string") do
-		expect("Yeah, yeah, yeah".word_count("yeah")).to(eq("'Yeah' occurs in 'Yeah, yeah, yeah' 3 times."))
+		expect("Yeah, yeah, yeah".word_count("yeah")).to(eq("\"Yeah\" occurs in \"Yeah, yeah, yeah\" 3 times."))
 	end
 	it("reports when no instances of the word are found in the string") do
-		expect("Yeah, yeah, yeah".word_count("no")).to(eq("'No' does not occur in 'Yeah, yeah, yeah'"))
+		expect("Yeah, yeah, yeah".word_count("no")).to(eq("\"No\" does not occur in \"Yeah, yeah, yeah\""))
 	end
 end
